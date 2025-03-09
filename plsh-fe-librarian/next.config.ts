9e879649a09ce4d2a38ac864ab033056ec5d5117
Viet <110122858+Viet170916@users.noreply.github.com>
2025-03-10 01:27:01 +0700

    update

--- plsh-fe-librarian/next.config.ts
+++ plsh-fe-librarian/next.config.ts
@@ -1,7 +1,12 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.externals = [...config.externals, "canvas"];
+        }
+        return config;
+    },
     trailingSlash: false,
     reactStrictMode: true,
     typescript: {
